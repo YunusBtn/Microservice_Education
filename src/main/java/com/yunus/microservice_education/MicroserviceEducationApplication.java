b1b38d419c46af725ef7d2f2ad5216342f654c82
Yunus Emre <yunusbtn43@gmail.com>
2026-06-28 00:19:16 +0300

    start

---
+++ src/main/java/com/yunus/microservice_education/MicroserviceEducationApplication.java
@@ -0,0 +1,13 @@
+package com.yunus.microservice_education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceEducationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceEducationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yunus/microservice_education/MicroserviceEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunus.microservice_education;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEducationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
