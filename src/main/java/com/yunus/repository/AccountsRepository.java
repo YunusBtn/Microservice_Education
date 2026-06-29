d84b5653589965494789f492835ba113404d3bb4
Yunus Emre <yunusbtn43@gmail.com>
2026-06-29 17:53:31 +0300

    updated

--- src/main/java/com/yunus/repository/AccountsRepository.java
+++ src/main/java/com/yunus/repository/AccountsRepository.java
@@ -1,6 +1,11 @@
 package com.yunus.repository;
 
+import com.yunus.entity.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
     Optional<Accounts> findByCustomerId(Long customerId);
