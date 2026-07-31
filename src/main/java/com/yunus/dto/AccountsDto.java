c6f10b16a5ffcab223f1b3b44c76ee24615432e0
Yunus Emre <yunusbtn43@gmail.com>
2026-07-31 22:10:42 +0300

    import deleted

--- src/main/java/com/yunus/dto/AccountsDto.java
+++ src/main/java/com/yunus/dto/AccountsDto.java
@@ -1,9 +1,5 @@
 package com.yunus.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
-import lombok.Data;
 
 @Data
 @Schema(
