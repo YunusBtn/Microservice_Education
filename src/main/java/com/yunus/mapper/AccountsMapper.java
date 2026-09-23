eb966bf9a03123c34987286044e01f51a398fdb1
Yunus Emre <yunusbtn43@gmail.com>
2026-09-23 23:36:02 +0300

    Account

---
+++ src/main/java/com/yunus/mapper/AccountsMapper.java
@@ -0,0 +1,24 @@
+package com.yunus.mapper;
+
+
+import com.yunus.dto.AccountsDto;
+import com.yunus.entity.Accounts;
+
+public class AccountsMapper {
+
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    //Static method to map AccountsDto to Accounts
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+
+}
